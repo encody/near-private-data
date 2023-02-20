36c8bf018545e4faba1e131efa8f64958eb4da8b
Jacob <encody@users.noreply.github.com>
2023-02-20 16:23:03 +0900

    feat: key repository contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,84 @@
-mod contract;
-pub use contract::*;
+use near_sdk::{
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    env, near_bindgen,
+    store::LookupMap,
+    AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+};
+use near_sdk_contract_tools::{event, standard::nep297::Event};
 
-#[allow(dead_code, unused)]
-#[cfg(all(test, not(target_arch = "wasm32")))]
-mod tests {
-    use near_sdk::{test_utils::*, testing_env, AccountId, ONE_NEAR};
+#[allow(unused)]
+#[derive(BorshStorageKey, BorshSerialize)]
+enum StorageKey {
+    KeyMap,
+}
 
-    fn contract_account() -> AccountId {
-        "contract".parse::<AccountId>().unwrap()
+#[event(
+    standard = "x-public-key-repository",
+    version = "1.0.0",
+    serde = "near_sdk::serde"
+)]
+enum PublicKeyRepositoryEvent {
+    PublicKeyChange {
+        account_id: AccountId,
+        public_key: Option<String>,
+    },
+}
+
+#[near_bindgen]
+#[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
+pub struct PublicKeyRepositoryContract {
+    key_map: LookupMap<AccountId, String>,
+}
+
+#[near_bindgen]
+impl PublicKeyRepositoryContract {
+    #[init]
+    pub fn new() -> Self {
+        Self {
+            key_map: LookupMap::new(StorageKey::KeyMap),
+        }
     }
 
-    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder
-            .current_account_id(contract_account())
-            .account_balance(15 * ONE_NEAR)
-            .signer_account_id(predecessor_account_id.clone())
-            .predecessor_account_id(predecessor_account_id);
-        builder
+    pub fn get_public_key(&self, account_id: AccountId) -> Option<&String> {
+        self.key_map.get(&account_id)
     }
 
-    #[test]
-    fn test() {}
+    #[payable]
+    pub fn set_public_key(&mut self, public_key: Option<String>) -> PromiseOrValue<()> {
+        near_sdk::assert_one_yocto();
+        let initial_storage_usage = env::storage_usage();
+
+        self.key_map
+            .set(env::predecessor_account_id(), public_key.clone());
+        self.key_map.flush();
+
+        PublicKeyRepositoryEvent::PublicKeyChange {
+            account_id: env::predecessor_account_id(),
+            public_key,
+        }
+        .emit();
+
+        let final_storage_usage = env::storage_usage();
+
+        let credit = if final_storage_usage < initial_storage_usage {
+            env::attached_deposit()
+                + (initial_storage_usage - final_storage_usage) as u128 * env::storage_byte_cost()
+        } else {
+            let charge =
+                (final_storage_usage - initial_storage_usage) as u128 * env::storage_byte_cost();
+            env::attached_deposit()
+                .checked_sub(charge)
+                .unwrap_or_else(|| {
+                    env::panic_str(&format!("Requires deposit of at least {charge} yoctoNEAR"))
+                })
+        };
+
+        if credit > 0 {
+            Promise::new(env::predecessor_account_id())
+                .transfer(credit)
+                .into()
+        } else {
+            PromiseOrValue::Value(())
+        }
+    }
 }
