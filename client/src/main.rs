6e67f1e64c5dac47d06f8850dd7cf81bceb908b4
Jacob <encody@users.noreply.github.com>
2023-03-01 04:02:33 +0900

    feat: interactive demo chat

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,21 @@
-use std::{path::PathBuf, sync::Arc};
+use std::{
+    path::PathBuf,
+    str::FromStr,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
 
 use base64ct::{Base64, Encoding};
 
+use console::style;
 use near_jsonrpc_client::{NEAR_MAINNET_RPC_URL, NEAR_TESTNET_RPC_URL};
 use near_primitives::types::AccountId;
-use rand::rngs::OsRng;
 use serde::{Deserialize, Serialize};
+use x25519_dalek::StaticSecret;
 
-use crate::{key_registry::KeyRegistry, messenger::Messenger, wallet::Wallet};
+use crate::{messenger::Messenger, wallet::Wallet};
 
 pub mod channel;
 pub mod key_registry;
@@ -18,8 +26,8 @@ pub mod wallet;
 #[derive(Serialize, Deserialize, Debug)]
 struct Environment {
     key_file_path: PathBuf,
-    key2_file_path: PathBuf,
     network: Option<String>,
+    messenger_secret_key: String,
     key_registry_account_id: AccountId,
     message_repository_account_id: AccountId,
 }
@@ -34,6 +42,36 @@ fn network_rpc_url(network: Option<String>) -> String {
         .unwrap_or_else(|| NEAR_TESTNET_RPC_URL.to_string())
 }
 
+// message receiver thread
+fn receiver(messenger: Arc<Messenger>, sender_id: AccountId) -> impl Fn() {
+    let alive = Arc::new(AtomicBool::new(true));
+
+    let kill = {
+        let alive = Arc::clone(&alive);
+        move || {
+            alive.store(false, Ordering::SeqCst);
+        }
+    };
+
+    tokio::spawn({
+        async move {
+            while alive.load(Ordering::SeqCst) {
+                if let Some(received_message) =
+                    messenger.receive_one_from(&sender_id).await.unwrap()
+                {
+                    eprintln!(
+                        "{}: {}",
+                        style(&sender_id).red().bright(),
+                        String::from_utf8_lossy(&received_message)
+                    );
+                }
+            }
+        }
+    });
+
+    kill
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv()?;
@@ -41,116 +79,91 @@ async fn main() -> anyhow::Result<()> {
     let env: Environment = envy::from_env()?;
 
     let signer = near_crypto::InMemorySigner::from_file(&env.key_file_path)?;
-    let signer2 = near_crypto::InMemorySigner::from_file(&env.key2_file_path)?;
 
-    let wallet1 = Arc::new(Wallet::new(
+    let wallet = Arc::new(Wallet::new(
         network_rpc_url(env.network.clone()),
         signer.account_id.clone(),
         signer,
     ));
-    let wallet2 = Arc::new(Wallet::new(
-        network_rpc_url(env.network.clone()),
-        signer2.account_id.clone(),
-        signer2,
+
+    let messenger_secret_key: [u8; 32] = Base64::decode_vec(&env.messenger_secret_key)
+        .unwrap()
+        .try_into()
+        .unwrap();
+
+    let messenger = Arc::new(Messenger::new(
+        Arc::clone(&wallet),
+        StaticSecret::from(messenger_secret_key),
+        &env.key_registry_account_id,
+        &env.message_repository_account_id,
     ));
 
-    let keyreg1 = KeyRegistry::new(Arc::clone(&wallet1), &env.key_registry_account_id);
-    let keyreg2 = KeyRegistry::new(Arc::clone(&wallet2), &env.key_registry_account_id);
+    eprintln!("Welcome to the {} (test version)", style("NEAR Private Data Messenger").magenta());
 
-    let rng = OsRng;
-
-    let secret_key1 = x25519_dalek::StaticSecret::new(rng);
-    let public_key = x25519_dalek::PublicKey::from(&secret_key1);
-
-    let public_key_string = Base64::encode_string(public_key.as_bytes());
-
-    eprintln!("Generated my key: {public_key_string}");
-
-    eprint!("Setting my key in registry...");
-    keyreg1.set_my_key(&public_key).await?;
+    eprint!("Syncing public key with key repository...");
+    messenger.sync_key().await?;
     eprintln!("done.");
 
-    eprint!("Retrieving my key from registry...");
-    let response = keyreg1.get_my_key().await?;
-    eprintln!("done.");
+    loop {
+        println!(
+            "You are logged in as {}.",
+            style(&wallet.account_id).cyan().bright(),
+        );
+        println!("Enter {} to exit.", style("/quit").bright().bold());
 
-    eprintln!(
-        "Response from contract: {}",
-        Base64::encode_string(&response),
-    );
-
-    assert_eq!(public_key.as_bytes() as &[u8], &response);
-
-    eprintln!("Key is correct.");
-
-    eprint!("Setting second account key in registry...");
-    let secret_key2 = x25519_dalek::StaticSecret::new(rng);
-    let public_key2 = x25519_dalek::PublicKey::from(&secret_key2);
-    keyreg2.set_my_key(&public_key2).await?;
-    eprintln!("done.");
-
-    let message = b"first to second";
-
-    eprint!("Creating messengers...");
-    let mut messenger1 = Messenger::new(
-        Arc::clone(&wallet1),
-        secret_key1,
-        &env.key_registry_account_id,
-        &env.message_repository_account_id,
-    );
-    let mut messenger2 = Messenger::new(
-        Arc::clone(&wallet2),
-        secret_key2,
-        &env.key_registry_account_id,
-        &env.message_repository_account_id,
-    );
-    eprintln!("done.");
-
-    eprint!("Registering correspondents...");
-    messenger2
-        .register_correspondent(&wallet1.account_id)
-        .await?;
-    messenger1
-        .register_correspondent(&wallet2.account_id)
-        .await?;
-    eprintln!("done.");
-
-    eprintln!(
-        "Sending \"{}\" from {} to {}...",
-        String::from_utf8_lossy(message),
-        &wallet1.account_id,
-        &wallet2.account_id,
-    );
-    let start_time = std::time::Instant::now();
-
-    // message receiver thread
-    let handle = tokio::spawn({
-        let receiver_id = wallet2.account_id.clone();
-        let sender_id = wallet1.account_id.clone();
-        async move {
-            eprintln!("Checking for new messages to {}...", &receiver_id);
-            let received_message = loop {
-                if let Some(m) = messenger2
-                    .check_received_one_from(&sender_id)
-                    .await
-                    .unwrap()
-                {
-                    break m;
+        let correspondent: String = dialoguer::Input::new()
+            .with_prompt("Chat with")
+            .validate_with(|s: &String| {
+                if s == "/quit" || AccountId::from_str(s).is_ok() {
+                    Ok(())
+                } else {
+                    Err("Invalid")
                 }
-            };
-            eprintln!(
-                "Received: \"{}\"",
-                String::from_utf8_lossy(&received_message),
-            );
-            eprintln!("Time to receive: {}ms", start_time.elapsed().as_millis());
+            })
+            .interact_text()
+            .unwrap();
+
+        if correspondent == "/quit" {
+            return Ok(());
         }
-    });
 
-    messenger1.send(&wallet2.account_id, message).await?;
+        let correspondent = AccountId::from_str(&correspondent).unwrap();
 
-    handle.await?;
+        eprintln!("Enter {} to leave chat.", style("/leave").bright().bold());
 
-    eprintln!("Completed.");
+        messenger
+            .register_correspondent(&correspondent)
+            .await
+            .unwrap();
 
-    Ok(())
+        let kill = receiver(Arc::clone(&messenger), correspondent.clone());
+
+        loop {
+            let message: String = dialoguer::Input::new()
+                .with_prompt("say")
+                .with_post_completion_text("you")
+                .interact_text()
+                .unwrap();
+
+            if message == "/leave" {
+                kill();
+                break;
+            }
+
+            messenger.send(&correspondent, message).await.unwrap();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use base64ct::{Base64, Encoding};
+    use rand::rngs::OsRng;
+
+    #[test]
+    fn generate_messenger_secret_key() {
+        let messenger_secret_key = x25519_dalek::StaticSecret::new(OsRng);
+        let secret_key_b64 = Base64::encode_string(&messenger_secret_key.to_bytes());
+        println!("\"{secret_key_b64}\"");
+    }
 }
--- client/src/messenger.rs
+++ client/src/messenger.rs
@@ -1,9 +1,14 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
 use anyhow::{anyhow, bail};
+use base64ct::{Base64, Encoding};
+use console::style;
 use near_primitives::types::AccountId;
-use tokio::try_join;
-use x25519_dalek::StaticSecret;
+use tokio::{sync::RwLock, try_join};
+use x25519_dalek::{PublicKey, StaticSecret};
 
 use crate::{
     channel::{Channel, SequenceHash},
@@ -16,7 +21,7 @@ pub struct Messenger {
     secret_key: StaticSecret,
     key_registry: KeyRegistry,
     message_repository: MessageRepository,
-    conversations: HashMap<AccountId, Conversation>,
+    conversations: Arc<RwLock<HashMap<AccountId, Conversation>>>,
 }
 
 impl Messenger {
@@ -30,26 +35,32 @@ impl Messenger {
             secret_key: messenger_secret_key,
             key_registry: KeyRegistry::new(Arc::clone(&wallet), key_registry_account_id),
             message_repository: MessageRepository::new(wallet, message_repository_account_id),
-            conversations: HashMap::new(),
+            conversations: Default::default(),
         }
     }
 
-    pub async fn register_correspondent(&mut self, account_id: &AccountId) -> anyhow::Result<()> {
+    pub async fn sync_key(&self) -> anyhow::Result<()> {
+        self.key_registry
+            .set_my_key(&PublicKey::from(&self.secret_key))
+            .await
+    }
+
+    pub async fn register_correspondent(&self, account_id: &AccountId) -> anyhow::Result<()> {
         let correspondent_public_key = self.key_registry.get_key_for(account_id).await?;
         let correspondent_public_key: [u8; 32] = match correspondent_public_key.try_into() {
             Ok(a) => a,
             Err(e) => bail!("Invalid key length {}", e.len()),
         };
         let (send, recv) = Channel::pair(&self.secret_key, &correspondent_public_key.into());
-        let mut send = Thread {
+        let send = Thread {
             channel: send,
             _correspondent: account_id.clone(),
-            next_nonce: 0,
+            next_nonce: Default::default(),
         };
-        let mut recv = Thread {
+        let recv = Thread {
             channel: recv,
             _correspondent: account_id.clone(),
-            next_nonce: 0,
+            next_nonce: Default::default(),
         };
 
         try_join!(
@@ -58,43 +69,46 @@ impl Messenger {
         )?;
 
         self.conversations
+            .write()
+            .await
             .insert(account_id.clone(), Conversation { send, recv });
         Ok(())
     }
 
     pub async fn send(
-        &mut self,
+        &self,
         recipient_id: &AccountId,
         cleartext: impl AsRef<[u8]>,
     ) -> anyhow::Result<()> {
-        let conversation = self
-            .conversations
-            .get_mut(recipient_id)
+        let conversations = self.conversations.read().await;
+        let conversation = conversations
+            .get(recipient_id)
             .ok_or_else(|| anyhow!("Unknown recipient: {}", recipient_id))?;
-        let nonce = conversation.send.next_nonce;
+        let nonce = conversation.send.next_nonce();
         let sequence_hash = conversation.send.next_sequence_hash();
         let ciphertext = conversation
             .send
             .channel
             .encrypt(nonce, cleartext.as_ref())?;
+        eprintln!(
+            "Sending message with sequence hash {}",
+            style(Base64::encode_string(&*sequence_hash)).yellow()
+        );
         self.message_repository
             .publish_message(&*sequence_hash, &ciphertext)
             .await?;
 
-        conversation.send.next_nonce += 1;
+        conversation.send.advance_nonce();
 
         Ok(())
     }
 
-    pub async fn check_received_one_from(
-        &mut self,
-        sender_id: &AccountId,
-    ) -> anyhow::Result<Option<Vec<u8>>> {
-        let conversation = self
-            .conversations
-            .get_mut(sender_id)
+    pub async fn receive_one_from(&self, sender_id: &AccountId) -> anyhow::Result<Option<Vec<u8>>> {
+        let conversations = self.conversations.read().await;
+        let conversation = conversations
+            .get(sender_id)
             .ok_or_else(|| anyhow!("Unknown recipient: {}", sender_id))?;
-        let nonce = conversation.recv.next_nonce;
+        let nonce = conversation.recv.next_nonce();
         let sequence_hash = conversation.recv.next_sequence_hash();
 
         let response = self.message_repository.get_message(&*sequence_hash).await?;
@@ -106,7 +120,7 @@ impl Messenger {
 
         let cleartext = conversation.recv.channel.decrypt(nonce, &ciphertext)?;
 
-        conversation.recv.next_nonce += 1;
+        conversation.recv.advance_nonce();
 
         Ok(Some(cleartext))
     }
@@ -115,19 +129,27 @@ impl Messenger {
 pub struct Thread {
     channel: Channel,
     _correspondent: AccountId,
-    next_nonce: u32,
+    next_nonce: Arc<Mutex<u32>>,
 }
 
 impl Thread {
-    pub async fn sync(&mut self, message_repository: &MessageRepository) -> anyhow::Result<()> {
-        self.next_nonce = message_repository
+    pub async fn sync(&self, message_repository: &MessageRepository) -> anyhow::Result<()> {
+        *self.next_nonce.lock().unwrap() = message_repository
             .discover_first_unused_nonce(&self.channel)
             .await?;
         Ok(())
     }
 
     pub fn next_sequence_hash(&self) -> SequenceHash {
-        self.channel.sequence_hash(self.next_nonce)
+        self.channel.sequence_hash(self.next_nonce())
+    }
+
+    pub fn next_nonce(&self) -> u32 {
+        *self.next_nonce.lock().unwrap()
+    }
+
+    pub fn advance_nonce(&self) {
+        *self.next_nonce.lock().unwrap() += 1;
     }
 }
 
