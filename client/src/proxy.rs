8cf23dc9bf03635fe806b04757e341f2284e56a9
Donovan Dall <awesomeibex@proton.me>
2023-06-27 11:55:47 +0100

    chore: add some sleepiness to the proxy

--- client/src/proxy.rs
+++ client/src/proxy.rs
@@ -1,21 +1,21 @@
 use crate::{
-    messenger::{Messenger},
-    network_rpc_url,
-    traits::Actor,
-    wallet::Wallet,
-    Base64String, SequencedHashMessage,
+    messenger::Messenger, network_rpc_url, traits::Actor, wallet::Wallet, Base64String,
+    SequencedHashMessage,
 };
 use anyhow::Result;
 use base64ct::{Base64, Encoding};
 use near_primitives::types::AccountId;
-use std::{path::{PathBuf, Path}, sync::Arc};
-use tokio::{
-    select,
-    sync::mpsc::{self, Sender},
-};
 use near_private_data_verification_gadget::{
     groth16::{prepare_verifying_key, VerifyingKey},
     Bls12, Proof,
+};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc, time::Duration,
+};
+use tokio::{
+    select,
+    sync::mpsc::{self, Sender}, time,
 };
 use x25519_dalek::StaticSecret;
 
@@ -77,7 +77,8 @@ impl Proxy {
             message_repository_account_id,
         );
 
-        let verifying_key = near_private_data_verification_gadget::read_vk(&config.verifying_key_path)?;
+        let verifying_key =
+            near_private_data_verification_gadget::read_vk(&config.verifying_key_path)?;
 
         Ok(Proxy {
             messenger,
@@ -96,33 +97,38 @@ impl Actor for Proxy {
 
         let vk = self.verifying_key.clone();
         Self::spawn(async move {
-            select! {
-                Some(message) = receiver.recv() => {
-                    // TODO: right now we act as a receiver of the message, proving that we can validate the message before we act as someone else
-                    // In the future we can: move the verifier onchain so that only good messages are posted OR
-                    // Verify in each client before they bother to look at the message, throw it away if it isnt valid
-    
-    
-                    // FIXME: unsafe
-                    let proof = Proof::<Bls12>::read_many(&message.preimage_proof, 1).unwrap()[0].clone();
-                    if !near_private_data_verification_gadget::verify(&proof, &message.hash[..], &prepare_verifying_key(&vk)) {
-                        log::debug!(
-                            "Could not verify proof for message {:?}",
-                            message.sequenced_message
-                        )
-                    } else {
-                        log::info!("Message passed validation, sending");
-                        if let Err(e) = self.messenger.message_repository
-                            .publish_message(&message.sequenced_message.sequence_hash, &message.sequenced_message.ciphertext)
-                        .await {
-                            log::error!("Failed to send raw sequenced message {:?}: {:?}", message.sequenced_message.sequence_hash, e);
+            let sleep = time::sleep(Duration::from_millis(50));
+            tokio::pin!(sleep);
+            loop {
+                select! {
+                    Some(message) = receiver.recv() => {
+                        // TODO: right now we act as a receiver of the message, proving that we can validate the message before we act as someone else
+                        // In the future we can: move the verifier onchain so that only good messages are posted OR
+                        // Verify in each client before they bother to look at the message, throw it away if it isnt valid
+
+
+                        // FIXME: unsafe
+                        let proof = Proof::<Bls12>::read_many(&message.preimage_proof, 1).unwrap()[0].clone();
+                        if !near_private_data_verification_gadget::verify(&proof, &message.hash[..], &prepare_verifying_key(&vk)) {
+                            log::debug!(
+                                "Could not verify proof for message {:?}",
+                                message.sequenced_message
+                            )
+                        } else {
+                            log::info!("Message passed validation, sending");
+                            if let Err(e) = self.messenger.message_repository
+                                .publish_message(&message.sequenced_message.sequence_hash, &message.sequenced_message.ciphertext)
+                            .await {
+                                log::error!("Failed to send raw sequenced message {:?}: {:?}", message.sequenced_message.sequence_hash, e);
+                            }
                         }
+
                     }
-    
+                    _ = &mut sleep => {
+                    }
                 }
             }
         })?;
         Ok(Arc::new(sender))
     }
-    
 }
